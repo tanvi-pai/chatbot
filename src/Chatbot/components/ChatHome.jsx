d6e4e575dd75b3185e044287fa5b900490806e90
tanvi-pai <tanvi.varma@prevalent.ai>
2023-01-13 09:39:41 +0530

    table ui

--- src/Chatbot/components/ChatHome.jsx
+++ src/Chatbot/components/ChatHome.jsx
@@ -7,10 +7,15 @@ import "./ChatHome.css"
 
 const ChatHome = () => {
   const [msgStore, setMsgStore] = useState([
-    { text: "how may i help", type: "bot" },
+    { text: "how may i help", type: "bot", data: [] },
   ]);
   const [userInput, setUserInput] = useState("");
   const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    formatTableData(["3.1.15","3.1.16","3.1.19","3.1.18","3.1.17"], ["0.89","0.89","0.87","0.87","0.87"])
+
+  },[])
   
   const onUserInput = (event) => {
     setUserInput(event.target.value);
@@ -18,7 +23,7 @@ const ChatHome = () => {
   
   const onSend = (event) => {
     event.preventDefault()
-    setMsgStore((prev) => [...prev, { text: userInput, type: "user" }]);
+    setMsgStore((prev) => [...prev, { text: userInput, type: "user", data: [] }]);
     setUserInput("")
     setLoading(true)
     getAPI();
@@ -26,16 +31,57 @@ const ChatHome = () => {
 
   const getAPI = () => {
     axios.get("http://192.168.9.83:5000/wpc?cei_count=5").then((response) => {
-      console.log("resppp", response.data)
+      console.log("resppp", response.data, JSON.stringify(response.data))
       let string = ""
-      if(response.data.cei_code.length > 1){
 
-      }else{
-        string = `${response.data.cei_code[0]}` + " is the worst perfoming CEI with " + `${response.data.failure_count[0] * 100}%` + "assets failuring this control."
+      switch(response.data.type){
+        case "worst_perfoming": {
+          string = `${response.data.cei_code[0]}` + " is the worst perfoming CEI with " + `${response.data.failure_count[0] * 100}%` + "assets failuring this control."
+        setMsgStore((prev) => [...prev, {text : string, type: "bot", data: []}])
+        break;
+        }
+        case "top_5": {
+          string = "Below listed are the worst performing CEIs"
+          setMsgStore((prev) => [...prev, {text : string, type: "bot", data: formatTableData(response.data.cei_code, response.data.failure_count)}])
+          break;
+        }
+        case "cei_status": {
+          string = ""
+           // setMsgStore((prev) => [...prev, {text : string, type: "bot", data: []}])
+          break
+        }
+        case "vulnerable_asset": {
+          string = ""
+           // setMsgStore((prev) => [...prev, {text : string, type: "bot", data: []}])
+          break;
+        }
+        default : {
+          string = "Can you try again"
+          // setMsgStore((prev) => [...prev, {text : string, type: "bot", data: []}])
       }
-      setMsgStore((prev) => [...prev, {text : string, type: "bot"}])
+        
+      }
+
     })
+    formatTableData(["3.1.15","3.1.16","3.1.19","3.1.18","3.1.17"], ["0.89","0.89","0.87","0.87","0.87"])
     // setLoading(false)
+  }
+
+  const formatTableData = (cei,failure_count) => {
+    let arr = []
+
+    cei.map((item, index) => {
+      console.log("item", item, index)
+      let obj = {
+        "cei": cei[index],
+        "failure": failure_count[index]
+      }
+      arr.push(obj)
+    })
+
+    console.log("arr", arr, JSON.stringify(arr))
+
+    return arr
   }
 
 
@@ -44,8 +90,8 @@ const ChatHome = () => {
       <Col xs={6}>
         <div className="d-flex justify-content-center">
           <Card className="loading-banner">
-            <Card.Header>Hello</Card.Header>
-            <Card.Body>
+            <Card.Header>CHATBOT</Card.Header>
+            <Card.Body className="msg-body dark-scroll">
               {loading}
               <MsgBubble msgStore={msgStore}/>
             </Card.Body>
@@ -66,7 +112,7 @@ const ChatHome = () => {
         </div>
       </Col>
       <Col xs={6}>
-        <Dashboard />
+        {/* <Dashboard /> */}
       </Col>
     </Stack>
   );
--- src/Chatbot/components/MsgBubble.jsx
+++ src/Chatbot/components/MsgBubble.jsx
@@ -1,4 +1,9 @@
+import { Table } from "react-bootstrap";
+
 const MsgBubble = (props) => {
+
+const data = [{"cei":"3.1.15","failure":"0.89"},{"cei":"3.1.16","failure":"0.89"},{"cei":"3.1.19","failure":"0.87"},{"cei":"3.1.18","failure":"0.87"},{"cei":"3.1.17","failure":"0.87"}]
+
   return (
     <>
       {props.msgStore.map((msg, id) => {
@@ -6,7 +11,30 @@ const MsgBubble = (props) => {
           <div key={id} className="message-container">
               <div className={msg.type === "bot" ? "bot-message" : "user-message"}>
               {msg.text}
-            </div>
+              </div>
+              {
+                msg.data && msg.data.length > 0 &&
+                <Table className="table" striped bordered hover size="sm" style={{marginLeft: "5px"}}>
+                  <thead>
+                    <tr>
+                      <th>CEI</th>
+                      <th>Failure count</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      msg.data.map((item,id) => {
+                        return(
+                          <tr key={id}>
+                          <td>{item.cei}</td>
+                          <td>{item.failure*100}%</td>
+                        </tr>
+                        )
+                      })
+                    }
+                  </tbody>
+                </Table>
+              }
           </div>
         );
       })}
